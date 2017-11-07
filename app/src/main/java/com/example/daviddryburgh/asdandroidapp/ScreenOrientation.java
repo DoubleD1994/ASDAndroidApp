836ac5ce66cde728c9c559d5e2d2e601d604a0d0
David Dryburgh <david.dryburgh1994@gmail.com>
2017-11-07 16:28:49 +0000

    Screen Orientation Update
    Screen Orientation Implemented

--- app/src/main/java/com/example/daviddryburgh/asdandroidapp/ScreenOrientation.java
+++ app/src/main/java/com/example/daviddryburgh/asdandroidapp/ScreenOrientation.java
@@ -1,21 +1,30 @@
 package com.example.daviddryburgh.asdandroidapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by daviddryburgh on 01/11/2017.
  */
 
 public class ScreenOrientation extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    Button btnCurrent, btnPortrait, btnLandscape, btnUnlock;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,54 @@ public class ScreenOrientation extends AppCompatActivity implements NavigationVi
 
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        btnCurrent = (Button) findViewById(R.id.current_orientation);
+        btnPortrait = (Button) findViewById(R.id.lock_portrait);
+        btnLandscape = (Button) findViewById(R.id.lock_landscape);
+        btnUnlock = (Button) findViewById(R.id.unlock_orientation);
+
+        btnCurrent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder = new AlertDialog.Builder(ScreenOrientation.this);
+                int orientation = ScreenOrientation.this.getResources().getConfiguration().orientation;
+                if(orientation == Configuration.ORIENTATION_PORTRAIT){
+                    builder.setMessage("Portrait Orientation");
+                }
+                else if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+                    builder.setMessage("Landscape Orientation");
+                }
+                else
+                {
+                    builder.setMessage("Other Orientation");
+                }
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        btnPortrait.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+            }
+        });
+
+        btnLandscape.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+            }
+        });
+
+        btnUnlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+            }
+        });
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
