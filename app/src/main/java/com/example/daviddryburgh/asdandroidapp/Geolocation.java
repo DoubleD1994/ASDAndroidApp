8efd8bbb2667d5787e95c2957ee825e175003f2c
David Dryburgh <david.dryburgh1994@gmail.com>
2017-11-08 16:35:06 +0000

    Geolocation Update
    Geolocation and Google Maps implemented

--- app/src/main/java/com/example/daviddryburgh/asdandroidapp/Geolocation.java
+++ app/src/main/java/com/example/daviddryburgh/asdandroidapp/Geolocation.java
@@ -1,8 +1,12 @@
 package com.example.daviddryburgh.asdandroidapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,12 +14,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by daviddryburgh on 01/11/2017.
  */
 
-public class Geolocation extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Geolocation extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+
+    private GoogleMap map;
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +43,7 @@ public class Geolocation extends AppCompatActivity implements NavigationView.OnN
         setContentView(R.layout.activity_geolocation);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
@@ -31,6 +51,31 @@ public class Geolocation extends AppCompatActivity implements NavigationView.OnN
 
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        mapView = (MapView) findViewById(R.id.map);
+        MapsInitializer.initialize(Geolocation.this);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        Button get_position = (Button) findViewById(R.id.get_position);
+
+        get_position.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                try {
+                    LocationManager locationManager = (LocationManager) Geolocation.this.getSystemService(Context.LOCATION_SERVICE);
+                    Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    LatLng initial = new LatLng(location.getLatitude(), location.getLongitude());
+                    map.addMarker(new MarkerOptions().position(initial));
+                    map.moveCamera(CameraUpdateFactory.newLatLng(initial));
+                } catch (SecurityException e){
+                    e.printStackTrace();
+                }
+            }
+        });
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -107,4 +152,13 @@ public class Geolocation extends AppCompatActivity implements NavigationView.OnN
         return true;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng initial = new LatLng(0, 0);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(initial));
+        mapView.onResume();
+
+    }
 }
